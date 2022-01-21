3c9d70b3f3f62efb3aed53f8926318805966fd87
Pratik Temkar <pratiktemkarofficial@gmail.com>
2022-01-21 15:11:41 +0530

    Hotfix UI 🚑️💄

---
+++ frontend/src/Components/prediction/Prediction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Prediction = () => {
+	return (
+		<>
+			<div className="container mt-2">
+				<h1>Prediction</h1>
+			</div>
+		</>
+	);
+};
+
+export default Prediction;
