d8206ddcb7603a22f4e86e6bb4522b938652ae01
Pratik Temkar <pratiktemkarofficial@gmail.com>
2022-01-21 22:37:56 +0530

    Leaflet Map Added.

--- frontend/src/Components/prediction/Prediction.js
+++ frontend/src/Components/prediction/Prediction.js
@@ -107,13 +107,28 @@ const Prediction = () => {
 								<MapContainer
 									center={[45.4, -75.7]}
 									zoom={13}
-									scrollWheelZoom={false}
+									scrollWheelZoom={true}
 									style={{ height: "calc(100vh - 80px)" }}
 								>
-									<TileLayer
-										url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-										attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-									/>
+									<Marker position={[45.4, -75.7]}>
+										<Popup>
+											A pretty CSS3 popup. <br /> Easily customizable.
+										</Popup>
+									</Marker>
+									<LayersControl position="topright">
+										<LayersControl.BaseLayer checked name="OSM">
+											<TileLayer
+												attribution="© OpenStreetMap contributors"
+												url="https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHJpeWFua2ExMjEwOTIiLCJhIjoiY2trbWQxY3h1MnBwMDJvbW5iNW96eTlrcCJ9.scUQlt1OTvRBxmshXeldaQ"
+											/>
+										</LayersControl.BaseLayer>
+										<LayersControl.BaseLayer name="Google Map">
+											<TileLayer
+												attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+												url="http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}&s=Ga"
+											/>
+										</LayersControl.BaseLayer>
+									</LayersControl>
 								</MapContainer>
 							</div>
 						</div>
